45c83ccb320b1ab865d1bc914ac2d28532f6bb51
ar0f <52024632+ar0f@users.noreply.github.com>
2022-02-06 20:35:06 +0900

    update

--- config.fish
+++ config.fish
@@ -1,16 +1,18 @@
-set -gx fish_prompt_pwd_dir_length 0
-set -g theme_display_group no
-#set -g theme_display_hostname no
-set -g theme_color_user 00ccff
-#set -g theme_display_rw no
-set -g theme_display_git no
+set fish_greeting
 
-alias python='python3'
-alias pip='pip3'
+set -gx fish_prompt_pwd_dir_length      0
+set -g theme_display_group              no
+#set -g theme_display_hostname          no
+#set -g theme_display_rw                no
+set -g theme_display_git                yes
+
+set -g theme_color_user                 00ccff
+set -g theme_color_host                 674EA7
+set -g theme_prompt_char_normal         ''
+set -g theme_prompt_char_superuser      '#'
 
 
-# Alias Default
-alias fishconfig='vim ~/.config/fish/config.fish'
+# Alias
 alias la='ls -alhtF'
 alias l='clear && ll'
 alias ..='cd ..'
@@ -19,8 +21,11 @@ alias ..3='cd ../../../'
 alias cp='cp -i'
 alias mv='mv -i'
 alias rm='rm -i'
+alias reboot='systemctl reboot'
 
-# Alias System
-alias ASLR_off='sudo sysctl -w kernel.randomize_va_space=0'
-alias ASLR_on='sudo sysctl -w kernel.randomize_va_space=2'
-alias SETENGDIR='LANG=C xdg-user-dirs-gtk-update' 
+# Alias Shortcut
+alias S_fish_config='vim ~/.config/fish/config.fish'
+alias S_vim_config='vim ~/.vimrc'
+alias S_i3_config='vim ~/.config/i3/config'
+alias pip='pip3'
+alias python='python3'
